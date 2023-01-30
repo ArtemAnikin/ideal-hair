b08a61c0eca9132be94b0d6904baad711e0279bf
anik_ad78 <Artem.anikin.2015@gmail.com>
2023-01-30 19:45:50 +0100

    add link

--- src/app/config/url.config.ts
+++ src/app/config/url.config.ts
@@ -8,7 +8,8 @@ export const getHomeUrl = '/'
 
 //footer links
 
-export const getInstagramUrl = '/instagram'
-export const getTelegramUrl = '/telegram'
-export const getViberUrl = '/viber'
-export const getPhoneUrl = '/phone'
+export const getInstagramUrl =
+	'https://instagram.com/ideal.hair_?igshid=YmMyMTA2M2Y='
+export const getTelegramUrl = 'https://t.me/idealhaair'
+export const getViberUrl = 'https://msng.link/o/?0665272340=vi'
+export const getPhoneUrl = 'tel:+380665272340'

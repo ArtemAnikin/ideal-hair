2c254ca0d9fc172108c13b9a6fe585400f3d7d7a
anik_ad78 <Artem.anikin.2015@gmail.com>
2023-01-30 03:04:08 +0100

    add assets

--- src/app/screens/Procedures/Procedures.tsx
+++ src/app/screens/Procedures/Procedures.tsx
@@ -1,7 +1,52 @@
-import { FC } from 'react'
+import cn from 'classnames'
+import { FC, useState } from 'react'
+
+import {
+	ProceduresData,
+	headerTitle
+} from '@/screens/Procedures/procedures.data'
+
+import Button from '@/ui/Button/Button'
+import Header from '@/ui/Header/Header'
+
+import styles from './Procedures.module.scss'
 
 const Procedures: FC = () => {
-	return <div>Procedures</div>
+	const [activeProcedure, setActiveProcedure] = useState<string>('')
+
+	const buttonHandler = (procedure: string) => {
+		if (procedure === activeProcedure) {
+			setActiveProcedure('')
+		} else {
+			setActiveProcedure(procedure)
+		}
+	}
+
+	return (
+		<div>
+			<Header title={headerTitle} showBackButton />
+
+			<div className={styles.procedures}>
+				{ProceduresData.map((procedure, index) => {
+					const key = `${procedure}-${index}`
+					return (
+						<div
+							className={cn(styles.procedure, {
+								[styles.active]: key === activeProcedure
+							})}
+							key={key}
+						>
+							<Button
+								title={procedure.title}
+								onClick={() => buttonHandler(key)}
+							/>
+							<div className={styles.description}>{procedure.description}</div>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
 
 export default Procedures

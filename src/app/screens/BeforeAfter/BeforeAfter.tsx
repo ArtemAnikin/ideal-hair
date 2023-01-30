3ee96057861a8304bfb0c2bb697736b859d2df3c
anik_ad78 <Artem.anikin.2015@gmail.com>
2023-01-30 03:18:46 +0100

    add before after

--- src/app/screens/BeforeAfter/BeforeAfter.tsx
+++ src/app/screens/BeforeAfter/BeforeAfter.tsx
@@ -1,7 +1,35 @@
+import Image from 'next/image'
 import { FC } from 'react'
 
+import {
+	BeforeAfterData,
+	headerTitle
+} from '@/screens/BeforeAfter/beforeAfter.data'
+
+import Header from '@/ui/Header/Header'
+
+import styles from './BeforeAfter.module.scss'
+
 const BeforeAfter: FC = () => {
-	return <div>BeforeAfter</div>
+	return (
+		<div>
+			<Header title={headerTitle} showBackButton />
+			<div className={styles.posters}>
+				{BeforeAfterData.map(poster => (
+					<div key={poster.alt} className={styles.poster}>
+						<Image
+							width={poster.width}
+							height={poster.height}
+							src={poster.src}
+							alt={poster.alt}
+							draggable={'false'}
+							priority
+						/>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default BeforeAfter
---
+++ src/app/screens/BeforeAfter/beforeAfter.data.ts
@@ -0,0 +1,33 @@
+import bg1 from '@/assets/beforeAfter/bg1.svg'
+import bg2 from '@/assets/beforeAfter/bg2.svg'
+import bg3 from '@/assets/beforeAfter/bg3.svg'
+
+interface Poster {
+	src: string
+	alt: string
+	width: number
+	height: number
+}
+
+export const headerTitle = 'ДО/ПІСЛЯ'
+
+export const BeforeAfterData: Poster[] = [
+	{
+		src: bg1,
+		alt: 'bg1',
+		width: 340,
+		height: 335
+	},
+	{
+		src: bg2,
+		alt: 'bg2',
+		width: 340,
+		height: 335
+	},
+	{
+		src: bg3,
+		alt: 'bg3',
+		width: 340,
+		height: 331
+	}
+]

e8f7e2c2d87ace92c5dd624f7c499e43b549cde6
anik_ad78 <Artem.anikin.2015@gmail.com>
2023-01-30 02:37:56 +0100

    add wy me

--- src/app/screens/WhyMe/WhyMe.tsx
+++ src/app/screens/WhyMe/WhyMe.tsx
@@ -1,7 +1,55 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { FC } from 'react'
 
+import { getHomeUrl } from '@/config/url.config'
+
+import {
+	backButton,
+	descriptionData,
+	profileData,
+	title
+} from '@/screens/WhyMe/whyMe.data'
+
+import styles from './WhyMe.module.scss'
+
 const WhyMe: FC = () => {
-	return <div>WhyMe</div>
+	const { push } = useRouter()
+
+	return (
+		<div>
+			<div className={styles.back} onClick={() => push(getHomeUrl)}>
+				<Image
+					width={backButton.width}
+					height={backButton.height}
+					src={backButton.src}
+					alt={backButton.alt}
+					draggable
+					unoptimized
+				/>
+			</div>
+
+			<div className={styles.top}>
+				<div className={styles.title}>{title}</div>
+				<Image
+					width={profileData.width}
+					height={profileData.height}
+					src={profileData.src}
+					alt={profileData.alt}
+					draggable={'false'}
+					priority
+				/>
+			</div>
+
+			<div className={styles.description}>
+				<ul>
+					{descriptionData.map(el => (
+						<li key={el}>{el}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	)
 }
 
 export default WhyMe

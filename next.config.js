7aa25b1249ce9b5b44563db334be73d73b059ec8
anik_ad78 <Artem.anikin.2015@gmail.com>
2023-01-31 23:22:00 +0100

    fix build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true
+	reactStrictMode: true,
+	experimental: {
+		appDir: true
+	}
+}
+
+module.exports = {
+	images: {
+		unoptimized: true
+	}
 }
 
 module.exports = nextConfig
